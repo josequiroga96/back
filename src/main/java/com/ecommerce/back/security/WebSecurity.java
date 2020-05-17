66e89d4370391333904007073879f62c62b5c375
JoseRojas96 <jose.rojas@ing.austral.edu.ar>
2020-05-16 21:36:45 -0300

    Removed unimportant import

--- src/main/java/com/ecommerce/back/security/WebSecurity.java
+++ src/main/java/com/ecommerce/back/security/WebSecurity.java
@@ -2,7 +2,6 @@ package com.ecommerce.back.security;
 
 import com.ecommerce.back.services.UserDetailsServiceImplementation;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
